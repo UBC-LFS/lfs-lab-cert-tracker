0fd0f5becdbf1415444114e499240412866ecaa0
David Jung <djungyvr@gmail.com>
2018-10-29 11:41:18 -0700

    Add lab detail link

--- lfs_lab_cert_tracker/auth_utils.py
+++ lfs_lab_cert_tracker/auth_utils.py
@@ -4,5 +4,4 @@ def is_admin(user):
     return user.groups.filter(name='admin').exists()
 
 def is_principal_investigator(user_id, lab_id):
-    user_lab = UserLab.objects.get(user=user_id, lab=lab_id)
-    return user_lab.role == UserLab.PRINCIPAL_INVESTIGATOR
+    return UserLab.objects.filter(user=user_id, lab=lab_id, role=UserLab.PRINCIPAL_INVESTIGATOR).exists()
