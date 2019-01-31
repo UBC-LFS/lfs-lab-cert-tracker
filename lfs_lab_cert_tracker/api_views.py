5f61eba894e539b6ba5318697e6998f1152a556b
David Jung <djungyvr@gmail.com>
2019-01-31 13:55:24 -0800

    Add logging to api level

--- lfs_lab_cert_tracker/api_views.py
+++ lfs_lab_cert_tracker/api_views.py
@@ -1,5 +1,7 @@
 import datetime
 
+import logging
+
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
@@ -13,6 +15,8 @@ from lfs_lab_cert_tracker.redirect_utils import handle_redirect
 Provides HTTP endpoints to access the api
 """
 
+logger = logging.getLogger(__name__)
+
 @login_required
 @admin_only
 @handle_redirect
@@ -20,6 +24,7 @@ Provides HTTP endpoints to access the api
 def certs(request, cert_id=None):
     data = request.POST
     res = api.create_cert(data['name'])
+    logger.info("Created cert %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -29,6 +34,7 @@ def certs(request, cert_id=None):
 def delete_certs(request, cert_id=None):
     data = request.POST
     res = api.delete_cert(cert_id)
+    logger.info("Deleted cert %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -38,6 +44,7 @@ def delete_certs(request, cert_id=None):
 def labs(request, lab_id=None):
     data = request.POST
     res = api.create_lab(data['name'])
+    logger.info("Created lab %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -47,6 +54,7 @@ def labs(request, lab_id=None):
 def delete_labs(request, lab_id=None):
     data = request.POST
     res = api.delete_lab(lab_id)
+    logger.info("Deleted lab %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -56,6 +64,7 @@ def delete_labs(request, lab_id=None):
 def lab_certs(request, lab_id=None, cert_id=None):
     data = request.POST
     res = api.create_lab_cert(lab_id, data['cert'])
+    logger.info("Created lab cert %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -65,6 +74,7 @@ def lab_certs(request, lab_id=None, cert_id=None):
 def delete_lab_certs(request, lab_id=None, cert_id=None):
     data = request.POST
     res = api.delete_lab_cert(lab_id, cert_id)
+    logger.info("Deleted lab cert %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -78,11 +88,12 @@ def user_certs(request, user_id=None, cert_id=None):
     if all([data['expiry_date_year'], data['expiry_date_month'], data['expiry_date_day']]):
         expiry_date = datetime.datetime(year=int(data['expiry_date_year']), month=int(data['expiry_date_month']), day=int(data['expiry_date_day']))
     res = api.update_or_create_user_cert(data['user'], data['cert'], files['cert_file'], expiry_date)
-    response = {
+    res = {
         'user_id': user_id,
         'cert_id': cert_id,
     }
-    return JsonResponse(response)
+    logger.info("Created user cert %s" % (res))
+    return JsonResponse(res)
 
 @login_required
 @user_or_admin
@@ -91,7 +102,7 @@ def user_certs(request, user_id=None, cert_id=None):
 def delete_user_certs(request, user_id=None, cert_id=None):
     data = request.POST
     res = api.delete_user_cert(user_id, cert_id)
-
+    logger.info("Deleted user cert %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -102,6 +113,7 @@ def user_labs(request, user_id=None, lab_id=None):
     data = request.POST
     user = api.get_user_by_cwl(data['user'])
     res = api.create_user_lab(user.id, lab_id, data['role'])
+    logger.info("Created user lab %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -111,6 +123,7 @@ def user_labs(request, user_id=None, lab_id=None):
 def delete_user_lab(request, user_id=None, lab_id=None):
     data = request.POST
     res = api.delete_user_lab(user_id, lab_id)
+    logger.info("Deleted user lab %s" % (res))
     return JsonResponse(res)
 
 @login_required
@@ -125,4 +138,5 @@ def users(request):
         email=data['email'],
         cwl=data['cwl'],
     )
+    logger.info("Creaetd user %s" % (res))
     return JsonResponse(res)
--- lfs_lab_cert_tracker/redirect_utils.py
+++ lfs_lab_cert_tracker/redirect_utils.py
@@ -1,5 +1,9 @@
+import logging
+
 from django.shortcuts import redirect
 from django.db.utils import IntegrityError
+
+logger = logging.getLogger(__name__)
 
 def handle_redirect(func):
     def hr(request, *args, **kwargs):
@@ -9,7 +13,9 @@ def handle_redirect(func):
                 return redirect(request.POST['redirect_url'])
             return res
         except IntegrityError as ie:
+            logger.error("IntegrityError: " + str(ie))
             return redirect('/error/%s'%("Invalid! Check if action already done"))
         except Exception as e:
+            logger.error("Exception: " + str(e))
             return redirect('/error/%s'%(e))
     return hr
