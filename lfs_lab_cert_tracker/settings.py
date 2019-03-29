e149f0568af55d3da27d43ce94735c10ad5a8a1a
David Jung <djungyvr@gmail.com>
2019-03-28 20:46:04 -0700

    Add environment variable config

--- lfs_lab_cert_tracker/settings.py
+++ lfs_lab_cert_tracker/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'DUMMY_KEY_CHANGE_IN_PRODUCTION'
+SECRET_KEY = os.environ['CERT_TRACKER_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['CERT_TRACKER_ENV'] != 'prod'
 
 ALLOWED_HOSTS = []
 
