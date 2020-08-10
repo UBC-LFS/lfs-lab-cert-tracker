6d32c1af7ba376390d59dab6f405a61a62ffda88
exponentian <exponentian.oh@gmail.com>
2020-08-10 09:30:42 -0700

    edited a title for mandatory training

--- email_notification/send_email_base.py
+++ email_notification/send_email_base.py
@@ -46,7 +46,7 @@ def html_template(first_name, last_name, message):
         <div>
             If you are trying to enroll in a missing or expired training, or to retrieve the training completion record, please visit the following links to get to the appropriate sites:
             <p>
-                <b>UBC mandatory training</b><br />
+                <b>UBC/LFS Mandatory Training</b><br />
                 <a href="https://my.landfood.ubc.ca/lfs-mandatory-training">https://my.landfood.ubc.ca/lfs-mandatory-training</a>
             </p>
         </div>
