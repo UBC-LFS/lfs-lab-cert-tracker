a2dbe444d37f128d48491dee6cd05e05579e3cc9
exponentian <exponentian.oh@gmail.com>
2022-12-16 12:15:49 -0800

    changed target to next

--- shibboleth/context_processors.py
+++ shibboleth/context_processors.py
@@ -5,8 +5,8 @@ from urllib.parse import quote
 def login_link(request):
     shib_login = reverse('shibboleth:login')
     full_path = quote(request.get_full_path())
-    print('login_link =====', shib_login, full_path, '{0}?target={1}'.format(shib_login, full_path))
+    print('login_link =====', shib_login, full_path, '{0}?next={1}'.format(shib_login, full_path))
     print(request.user, request.user.is_authenticated)
     return { 
-        'login_link': '{0}?target={1}'.format(shib_login, full_path)
+        'login_link': '{0}?next={1}'.format(shib_login, full_path)
     }
--- shibboleth/views.py
+++ shibboleth/views.py
@@ -5,12 +5,13 @@ from urllib.parse import quote
 
 
 class Shib_Login(View):
-    redirect_field_name = 'target'
+    redirect_field_name = 'next'
 
     def get(self, request, *args, **kwargs):
         target = request.GET.get(self.redirect_field_name, '')
-        login = settings.LOGIN_URL + '?target={0}'.format(quote(target))
+        login = settings.LOGIN_URL + '?next={0}'.format(quote(target))
 
-        print('Shib_Login =====', login)
+        print('Shib_Login =====', login, request.GET.get('next'))
         print(request.user, request.user.is_authenticated)
+
         return redirect(login)
