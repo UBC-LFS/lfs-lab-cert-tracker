bf8ae4fe67994e7f7f7ad719e937bead5d943826
William <william.kang@ubc.ca>
2023-06-16 15:19:03 -0700

    Updated tests

--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -8,6 +8,7 @@ import json
 from app.utils import Api
 from app.tests.test_users import LOGIN_URL, ContentType, DATA, USERS, PASSWORD
 
+import concurrent.futures
 
 class AreaTest(TestCase):
     fixtures = DATA
@@ -15,20 +16,49 @@ class AreaTest(TestCase):
     def setUp(self):
         self.api = Api()
     
-    def test_api_call(self):
-        print('\n- Test: check api call for one user')
-        cwl = "wtamagi"
+    # def test_api_call(self):
+    #     print('\n- Test: check api call for one user')
+    #     cwl = ""
         
-        result = self.api.get_certificates_for_user(cwl)
-        self.assertEqual(result.status_code, 200)
-        print("RESULT IS", result.json())
+    #     result = self.api.get_certificates_for_user(cwl)
+    #     # Convert JSON string to Python object
+    #     # data = json.loads(result)
+
+    #     # Pretty print the JSON object #imeldac
+    #     # pretty_json = json.dumps(data, indent=4)
+    #     # print(pretty_json)
+    #     results = json.dumps({'results': result})
+    #     print(results)
+    
+    def test_api_threads(self):
+        cwl_list = [] 
+
+        # Maximum number of concurrent threads
+        max_workers = 5
+        cwl_errors = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = [executor.submit(self.api.get_certificates_for_user, cwl) for cwl in cwl_list]
+
+            for future, cwl in zip(futures, cwl_list):
+                try:
+                    result = future.result()  # Get the result of the completed future
+                    print(result)
+                except Exception as e:
+                    print(f"An error occurred for CWL {cwl}: {str(e)}")
+                    cwl_errors.append(cwl)
+
+        # Print the list of CWLs with errors
+        print("CWLs with errors:", cwl_errors)
         
     # def test_api_call_multiple_cwls(self):
     #     print('\n- Test: check api call for multiple users')
-    #     cwls = ["wtamagi", "wkang01", "hs55555"]
+    #     cwls = []
         
-    #     result = self.api.get_certificates_for_cwls(cwls)
+    #     result = self.api.get_certificates_for_cwls_at_once(cwls)
     #     print("RESULT IS", result)
+    #     print("LENGTH IS", len(result))
+
         
         
         
