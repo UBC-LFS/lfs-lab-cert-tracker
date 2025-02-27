92b256539b781d0a4b5f0f5c1494e271441d9872
exponentian <exponentian.oh@gmail.com>
2025-02-27 12:40:41 -0800

    added area details and settings views

---
+++ app/settings_views.py
@@ -0,0 +1,397 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
+from django.views.decorators.cache import cache_control, never_cache
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.core.exceptions import SuspiciousOperation
+
+from datetime import datetime, date, timedelta
+
+from .accesses import *
+from .forms import *
+from .utils import *
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class Index(LoginRequiredMixin, View):
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/settings/index.html', {
+            'last_ten_users': User.objects.all().order_by('-date_joined')[:20]
+        })
+    
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class AllAreas(LoginRequiredMixin, View):
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        area_list = Lab.objects.all()
+
+        # Pagination enables
+        query = request.GET.get('q')
+        if query:
+            area_list = Lab.objects.filter( Q(name__icontains=query) ).distinct()
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(area_list, NUM_PER_PAGE)
+
+        try:
+            areas = paginator.page(page)
+        except PageNotAnInteger:
+            areas = paginator.page(1)
+        except EmptyPage:
+            areas = paginator.page(paginator.num_pages)
+
+        # Add a number of users in each area
+        for area in areas:
+            area.num_certs = area.labcert_set.count()
+            area.num_users = area.userlab_set.count()
+
+        return render(request, 'app/settings/all_areas.html', {
+            'areas': areas,
+            'total_areas': len(area_list)
+        })
+
+
+class CreateArea(LoginRequiredMixin, View):
+    """ Create a new area """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/settings/create_area.html', {
+            'form': AreaForm(),
+            'recent_areas': Lab.objects.all().order_by('-id')[:20]
+        })
+
+    @method_decorator(require_POST)
+    def post(self, request, *args, **kwargs):
+        form = AreaForm(request.POST)
+        if form.is_valid():
+            area = form.save()
+            if area:
+                messages.success(request, 'Success! {0} has been created.'.format(area.name))
+            else:
+                messages.error(request, 'Error! Failed to create {0}.'.format(area.name))
+        else:
+            messages.error(request, 'Error! Form is invalid. {0}'.format(get_error_messages(form.errors.get_json_data())))
+
+        return redirect('app:create_area')
+
+
+
+
+@login_required(login_url=settings.LOGIN_URL)
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@access_admin_only
+@require_http_methods(['POST'])
+def edit_area(request):
+    """ Update the name of area """
+
+    area = get_lab_by_id(request.POST.get('area'))
+    form = AreaForm(request.POST, instance=area)
+    if form.is_valid():
+        updated_area = form.save()
+        if updated_area:
+            messages.success(request, 'Success! {0} has been updated.'.format(updated_area.name))
+        else:
+            messages.error(request, 'Error! Failed to update {0}.'.format(area.name))
+    else:
+        messages.error(request, 'Error! Form is invalid. {0}'.format(get_error_messages(form.errors.get_json_data())))
+
+    return redirect('app:all_areas')
+
+
+@login_required(login_url=settings.LOGIN_URL)
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@access_admin_only
+@require_http_methods(['POST'])
+def delete_area(request):
+    """ Delete an area """
+
+    area = get_lab_by_id(request.POST.get('area'))
+    if area.delete():
+        messages.success(request, 'Success! {0} has been deleted.'.format(area.name))
+    else:
+        messages.error(request, 'Error! Failed to delete {0}.'.format(area.name))
+
+    return redirect('app:all_areas')
+
+
+# Training
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class AllTrainings(LoginRequiredMixin, View):
+    """ Display all training records """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        training_list = Cert.objects.all()
+
+        query = request.GET.get('q')
+        if query:
+            training_list = Cert.objects.filter( Q(name__icontains=query) ).distinct()
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(training_list, NUM_PER_PAGE)
+
+        try:
+            trainings = paginator.page(page)
+        except PageNotAnInteger:
+            trainings = paginator.page(1)
+        except EmptyPage:
+            trainings = paginator.page(paginator.num_pages)
+        
+        for cert in trainings:
+            cert.num_users = cert.usercert_set.count()
+
+        return render(request, 'app/settings/all_trainings.html', {
+            'total_trainings': len(training_list),
+            'trainings': trainings
+        })
+
+
+class CreateTraining(LoginRequiredMixin, View):
+    """ Create a new training """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/settings/create_training.html', {
+            'form': TrainingForm(),
+            'recent_trainings': Cert.objects.all().order_by('-id')[:20]
+        })
+
+    @method_decorator(require_POST)
+    def post(self, request, *args, **kwargs):
+        form = TrainingForm(request.POST)
+        if form.is_valid():
+            cert = form.save()
+            if cert:
+                messages.success(request, 'Success! {0} has been created.'.format(cert.name))
+            else:
+                messages.error(request, 'Error! Failed to create {0}. This training has already existed.'.format(cert.name))
+        else:
+            messages.error(request, 'Error! Form is invalid. {0}'.format(get_error_messages(form.errors.get_json_data())))
+
+        return redirect('app:create_training')
+
+
+# User
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class AllUsers(LoginRequiredMixin, View):
+    """ Display all users """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+
+        # if session has next value, delete it
+        if request.session.get('next'):
+            del request.session['next']
+
+        user_list = get_users()
+
+        # Pagination enables
+        query = request.GET.get('q')
+        if query:
+            user_list = User.objects.filter(
+                Q(username__icontains=query) | Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            ).order_by('id').distinct()
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(user_list, NUM_PER_PAGE)
+
+        try:
+            users = paginator.page(page)
+        except PageNotAnInteger:
+            users = paginator.page(1)
+        except EmptyPage:
+            users = paginator.page(paginator.num_pages)
+
+        for user in users:
+            user.missing_certs = get_user_missing_certs(user.id)
+            user.inactive = None
+            user_inactive = UserInactive.objects.filter(user_id=user.id)
+            if user_inactive.exists():
+                user.inactive = user_inactive.first()
+
+        areas = []
+        for area in Lab.objects.all():
+            area.has_lab_users = area.userlab_set.filter(role=UserLab.LAB_USER).values_list('user_id', flat=True)
+            area.has_pis = area.userlab_set.filter(role=UserLab.PRINCIPAL_INVESTIGATOR).values_list('user_id', flat=True)
+            areas.append(area)
+
+        return render(request, 'app/settings/all_users.html', {
+            'total_users': len(user_list),
+            'users': users,
+            'areas': areas,
+            'roles': { 
+                'LAB_USER': UserLab.LAB_USER, 
+                'PI': UserLab.PRINCIPAL_INVESTIGATOR 
+            }
+        })
+
+    @method_decorator(require_POST)
+    def post(self, request, *args, **kwargs):
+
+        # Edit a user
+        user = get_user_by_id(request.POST.get('user'))
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            if form.save():
+                messages.success(request, 'Success! {0} has been updated.'.format(user.get_full_name()))
+            else:
+                messages.error(request, 'Error! Failed to update {0}.'.format(user.get_full_name()))
+        else:
+            messages.error(request, 'Error! Form is invalid. {0}'.format(get_error_messages(form.errors.get_json_data())))
+
+        return HttpResponseRedirect( request.POST.get('next') )
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class CreateUser(LoginRequiredMixin, View):
+    """ Create a new user """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/settings/create_user.html', {
+            'form': UserForm(),
+            'recent_users': User.objects.all().order_by('-date_joined')[:20]
+        })
+
+    @method_decorator(require_POST)
+    def post(self, request, *args, **kwargs):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user:
+                if request.POST.get('send_email') == 'yes':
+                    email_error = None
+                    try:
+                        validate_email(user.email)
+                    except ValidationError as e:
+                         email_error = e
+
+                    if email_error is None:
+                        sent = send_info_email(user)
+                        if sent:
+                            messages.success(request, 'Success! {0} has been created and sent an email.'.format(user.get_full_name()))
+                        else:
+                            messages.warning(request, 'Warning! {0} has been created, but failed to send an email due to {1}'.format(user.get_full_name(), sent.error))
+                else:
+                    messages.success(request, 'Success! {0} has been created.'.format(user.get_full_name()))
+            else:
+                messages.error(request, 'Error! Failed to create {0}. Please check your CWL.'.format(user.get_full_name()))
+        else:
+            messages.error(request, 'Error! Form is invalid. {0}'.format(get_error_messages(form.errors.get_json_data())))
+
+        return redirect('app:create_user')
+
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class UserReportMissingTrainings(LoginRequiredMixin, View):
+    """ Display an user report for missing trainings """
+
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+
+        # Find users who have missing certs
+        user_list = []
+        for user in get_users('active'):
+            missing_certs = get_user_missing_certs(user.id)
+            if len(missing_certs) > 0: 
+                user.missing_certs = missing_certs
+                user_list.append(user)
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(user_list, 10)
+
+        try:
+            users = paginator.page(page)
+        except PageNotAnInteger:
+            users = paginator.page(1)
+        except EmptyPage:
+            users = paginator.page(paginator.num_pages)
+
+        return render(request, 'app/settings/user_report_missing_trainings.html', {
+            'total_users': len(user_list),
+            'users': users,
+            'download_user_report_missing_trainings_url': reverse('app:download_user_report_missing_trainings')
+        })
+
+
+@method_decorator([never_cache, access_admin_only], name='dispatch')
+class APIUpdates(LoginRequiredMixin, View):
+    """ Displays all the API updates made """
+    
+    @method_decorator(require_GET)
+    def get(self, request, *args, **kwargs):
+        
+        # if session has next value, delete it
+        if request.session.get('next'):
+            del request.session['next']
+
+        today = date.today()
+
+        user_cert_list = UserCert.objects.filter(by_api=True).order_by('uploaded_date', 'user__last_name', 'user__first_name')
+
+        stats = []
+        for i in range(6, 0, -1):
+            d = today - timedelta(i)
+            stats.append({
+                'date': convert_date_to_str(d),
+                'count': user_cert_list.filter(uploaded_date=d).count()
+            })
+        
+        date_from_q = request.GET.get('date_from')
+        date_to_q = request.GET.get('date_to')        
+        username_name_q = request.GET.get('q')
+        training_q = request.GET.get('training')
+
+        if bool(date_from_q) and bool(date_to_q):
+            user_cert_list = user_cert_list.filter( Q(uploaded_date__gte=date_from_q) & Q(uploaded_date__lte=date_to_q) )
+            today = '{0} ~ {1}'.format(date_from_q, date_to_q)
+        elif bool(date_from_q):
+            user_cert_list = user_cert_list.filter(uploaded_date=date_from_q)
+            today = date_from_q
+        elif bool(date_to_q):
+            user_cert_list = user_cert_list.filter(uploaded_date=date_to_q)
+            today = date_to_q
+        else:
+            user_cert_list = user_cert_list.filter(uploaded_date=today)
+            today = convert_date_to_str(today)
+        
+        if bool(username_name_q):
+            user_cert_list = user_cert_list.filter(
+                Q(user__username__icontains=username_name_q) | Q(user__first_name__icontains=username_name_q) | Q(user__last_name__icontains=username_name_q)
+            )
+        if bool(training_q):
+            user_cert_list = user_cert_list.filter(cert__name__icontains=training_q)
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(user_cert_list, NUM_PER_PAGE)
+
+        try:
+            user_certs = paginator.page(page)
+        except PageNotAnInteger:
+            user_certs = paginator.page(1)
+        except EmptyPage:
+            user_certs = paginator.page(paginator.num_pages)
+
+        return render(request, 'app/settings/api_updates.html', {
+            "total_user_certs": len(user_cert_list),
+            "user_certs": user_certs,
+            "today": today,
+            "stats": stats
+        })
+
--- app/urls.py
+++ app/urls.py
@@ -1,27 +1,47 @@
 from django.urls import path
+
 from . import views
+from . import settings_views
+from . import area_details_views
 
 app_name = 'app'
 
 urlpatterns = [
 
-    # Settings - classes
-    path('settings/', views.SettingIndex.as_view(), name='setting_index'),
-    
-    path('all-areas/', views.AllAreas.as_view(), name='all_areas'),
-    path('create-area/', views.CreateArea.as_view(), name='create_area'),
+    # Settings
 
-    path('all-trainings/', views.AllTrainings.as_view(), name='all_trainings'),
-    path('create-training/', views.CreateTraining.as_view(), name='create_training'),
+    path('settings/', settings_views.Index.as_view(), name='setting_index'),
     
-    path('all-users/', views.AllUsers.as_view(), name='all_users'),
-    path('create-user/', views.CreateUser.as_view(), name='create_user'),
-    path('user-report/', views.UserReportMissingTrainings.as_view(), name='user_report_missing_trainings'),
-    path('api-updates/', views.APIUpdates.as_view(), name='api_updates'),
+    path('all-areas/', settings_views.AllAreas.as_view(), name='all_areas'),
+    path('create-area/', settings_views.CreateArea.as_view(), name='create_area'),
+    path('func/area/update/', settings_views.edit_area, name='edit_area'),
+    path('func/area/delete/', settings_views.delete_area, name='delete_area'),
+
+    path('all-trainings/', settings_views.AllTrainings.as_view(), name='all_trainings'),
+    path('create-training/', settings_views.CreateTraining.as_view(), name='create_training'),
+    
+    path('all-users/', settings_views.AllUsers.as_view(), name='all_users'),
+    path('create-user/', settings_views.CreateUser.as_view(), name='create_user'),
+    path('user-report/', settings_views.UserReportMissingTrainings.as_view(), name='user_report_missing_trainings'),
+    path('api-updates/', settings_views.APIUpdates.as_view(), name='api_updates'),
+
+
+    # Area Details
+
+    path('areas/<int:area_id>/area-details/', area_details_views.Index.as_view(), name='area_details'),
+    path('areas/<int:area_id>/users-missing-training-record/', area_details_views.UsersMissingTrainings.as_view(), name='users_missing_trainings'),
+    path('areas/<int:area_id>/users-expired-training-record/', area_details_views.UsersExpiredTrainings.as_view(), name='users_expired_trainings'),
+    path('areas/<int:area_id>/add-user-to-area/', area_details_views.AddUserToArea.as_view(), name='add_user_to_area'),
+    path('areas/<int:area_id>/add-training-to-area/', area_details_views.AddTrainingToArea.as_view(), name='add_training_to_area'),
+
+    path('func/area/training/delete/', area_details_views.delete_training_in_area, name='delete_training_in_area'),
+    path('func/area/<int:area_id>/user/role/switch/', area_details_views.switch_user_role_in_area, name='switch_user_role_in_area'),
+    path('func/area/<int:area_id>/user/delete/', area_details_views.delete_user_in_area, name='delete_user_in_area'),
+
 
 
     # Users - classes
-    path('users/<int:user_id>/work-area/', views.UserAreasView.as_view(), name='user_areas'),
+    path('users/<int:user_id>/work-area/', views.UserAreas.as_view(), name='user_areas'),
     path('users/<int:user_id>/', views.UserDetails.as_view(), name='user_details'),
     path('users/<int:user_id>/my-training-record/', views.MyTrainingRecord.as_view(), name='my_training_record'),
     path('users/<int:user_id>/add-training-record/', views.AddTrainingRecord.as_view(), name='add_training_record'),
@@ -39,21 +59,7 @@ urlpatterns = [
     path('api/user/switch-inactive/', views.switch_inactive, name='switch_inactive'),
     path('api/users/assign/areas/', views.assign_user_areas, name='assign_user_areas'),
     path('api/users/<int:user_id>/read/welcome-message/', views.read_welcome_message, name="read_welcome_message"),
-
-
-    # Areas - classes
     
-    path('areas/<int:area_id>/', views.AreaDetailsView.as_view(), name='area_details'),
-
-
-    # Areas - api
-    path('api/area/update/', views.edit_area, name='edit_area'),
-    path('api/area/delete/', views.delete_area, name='delete_area'),
-    path('api/area/training/delete/', views.delete_training_in_area, name='delete_training_in_area'),
-    path('api/area/training/add/', views.add_training_area, name='add_training_area'),
-    path('api/area/<int:area_id>/user/role/switch/', views.switch_user_role_in_area, name='switch_user_role_in_area'),
-    path('api/area/<int:area_id>/user/delete/', views.delete_user_in_area, name='delete_user_in_area'),
-
 
     # Trainings - apis
     path('api/trainings/edit/', views.edit_training, name='edit_training'),
