44fe6a2da05318b85478030806a113668a46bdd5
exponentian <exponentian.oh@gmail.com>
2025-09-11 13:01:07 -0700

    added email notification

--- lfs_lab_cert_tracker/models.py
+++ lfs_lab_cert_tracker/models.py
@@ -114,7 +114,9 @@ class UserCert(models.Model):
             elif width > 1000 or height > 500:
                 width, height = width/1.5, height/1.5
 
-            img.thumbnail( (width, height), PILImage.ANTIALIAS )
+            # img.thumbnail( (width, height), PILImage.ANTIALIAS )
+            img.thumbnail( (width, height), PILImage.Resampling.LANCZOS ) # Pillow >= 10.0.0
+
             output = BytesIO()
             img.save(output, format='JPEG', quality=70) # Reduce a quality by 70%
             output.seek(0)
