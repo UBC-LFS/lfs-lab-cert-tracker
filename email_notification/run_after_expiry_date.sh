4d4b4bbd93ef0a20426e5f63cd42549ded983a24
exponentian <exponentian.oh@gmail.com>
2025-09-22 14:49:12 -0700

    updated gitignore

--- email_notification/run_after_expiry_date.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LOGFILE=/[DIRECTORY]/trms/email_notification/cron.log
-
-# source apache env
-. /etc/apache2/envvars
-
-echo "Run every 15 days - Started at $(date)" >> $LOGFILE 2>&1
-
-/[DIRECTORY]/venv/bin/python3 /[DIRECTORY]/trms/email_notification/after_expiry_date.py >> $LOGFILE 2>&1
--- email_notification/run_before_expiry_date.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LOGFILE=/[DIRECTORY]/trms/email_notification/cron.log
-
-. /etc/apache2/envvars
-
-echo "Run everyday - Started at $(date)" >> $LOGFILE 2>&1
-
-/[DIRECTORY]/venv/bin/python3 /[DIRECTORY]/trms/email_notification/before_expiry_date.py >> $LOGFILE 2>&1
