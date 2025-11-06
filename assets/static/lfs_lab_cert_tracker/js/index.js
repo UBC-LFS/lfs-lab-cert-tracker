f050fcc689c5684022696bfec13e8e363034eee1
afellhauer <afellhau@students.cs.ubc.ca>
2025-11-06 14:42:17 -0800

    [BUG FIX] Index.js correctly identifies which menu item to set as active
    - compares the href of the current pages to the href of the navigation link (removes query parameters)
    
    - this solution addresses the current bugs, but may require more normalization in order to correctly compare the current page to the static, navigation link

--- assets/static/lfs_lab_cert_tracker/js/index.js
+++ assets/static/lfs_lab_cert_tracker/js/index.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
-  const paths = window.location.pathname.split('/');
-  const len = paths.length;
-  const currPageName = paths[len - 2];
+  const path = window.location.href;
 
   const uls = $('#left-side-menu').find('ul');
   for (const ul of uls) {
     const hyperlinks = $(ul).find('a');
     for (const hyperlink of hyperlinks) {
-      $(hyperlink).parent().removeClass('active');
-
-      const slug = slugify($(hyperlink).text());
-      if (slug.includes(currPageName)) {
+      const href = hyperlink.href
+      if (compareWithoutQueryParams(href, path)) {
         $(hyperlink).parent().addClass('active');
+      } else {
+        $(hyperlink).parent().removeClass('active');
       }
+
     }
   }
 });
 
-function slugify(str) {
-  str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
-  str = str.toLowerCase(); // convert string to lowercase
-  str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
-           .replace(/\s+/g, '-') // replace spaces with hyphens
-           .replace(/-+/g, '-'); // remove consecutive hyphens
-  return str;
+// Basic normalization -> should cover most cases
+function compareWithoutQueryParams(href1, href2) {
+  href1 = href1.split("?")[0];
+  href2 = href2.split("?")[0];
+  return href1 === href2;
 }
+
+// function slugify(str) {
+//   str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+//   str = str.toLowerCase(); // convert string to lowercase
+//   str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+//            .replace(/\s+/g, '-') // replace spaces with hyphens
+//            .replace(/-+/g, '-'); // remove consecutive hyphens
+//   return str;
+// }
